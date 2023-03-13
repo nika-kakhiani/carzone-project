6fdaf43b97bdd2e53902042343582e570fef877d
User <nikakakhi@gmail.com>
2023-03-14 02:24:05 +0400

    corrected static

--- carzone/settings.py
+++ carzone/settings.py
@@ -162,3 +162,20 @@ EMAIL_USE_TLS = True
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
